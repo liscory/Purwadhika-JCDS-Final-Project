b33c67a871a12bd01f0aec1a3cde740f8f89ec12
ls-cy <liscory.ntr@gmail.com>
2021-05-21 01:53:50 +0700

    First commit

---
+++ Dashboard/app.py
@@ -0,0 +1,157 @@
+from flask import Flask, render_template, request
+import requests
+import joblib
+import pickle
+import plotly
+import plotly.graph_objs as go
+import json
+import pandas as pd 
+import numpy as np
+from model_plots import show_map, show_bar_ward, show_bar_ward_count, show_bar_grade, show_bar_grade_count, show_bar_usecode, show_line_sale 
+from mae_count import count_mae
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict(): 
+    df_residential = pd.read_csv('../Data/DF_Train.csv')
+
+    ward = sorted(df_residential['WARD'].unique().tolist())
+    quadrant = df_residential['QUADRANT'].unique().tolist()
+    heat = df_residential['HEAT'].unique().tolist()
+    style = sorted(df_residential['STYLE'].unique().tolist())
+    usecode = sorted(df_residential['USECODE'].unique().tolist())
+    saleyear = sorted(df_residential['SALEYEAR'].unique().tolist(), reverse=True)
+
+    house_spec = {
+        'ward':ward,
+        'quadrant':quadrant,
+        'heat':heat,
+        'style':style,
+        'usecode':usecode,
+        'saleyear':saleyear
+        }
+
+    return render_template('predict.html', spec=house_spec)
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    try:
+        print(request.method)
+        if request.method == 'POST':
+            input = request.form
+            
+            df_predict = pd.DataFrame({
+                'BATHRM' : [int(input['bathrooms'])],
+                'HF_BATHRM' : [int(input['half_bathrooms'])],
+                'ROOMS' : [int(input['rooms'])],
+                'BEDRM' : [int(input['bedrooms'])],
+                'ayb_age' : [int(input['building_age'])],
+                'eyb_age' : [int(input['renovation_years'])],
+                'GBA' : [float(input['gba'])],
+                'KITCHENS' : [int(input['kitchens'])],
+                'FIREPLACES' : [int(input['fireplaces'])],
+                'LANDAREA' : [float(input['landarea'])],
+                'LATITUDE' : [float(input['latitude'])],
+                'LONGITUDE' : [float(input['longitude'])],
+                'AC' : [input['ac']],
+                'QUALIFIED' : [input['qualified']],
+                'WARD' : [input['ward']],
+                'QUADRANT' : [input['quadrant']],
+                'HEAT' : [input['heat']],
+                'STYLE' : [input['style']],
+                'USECODE' : [input['usecode']],
+                'STRUCT' : [int(input['struct'])],
+                'GRADE' : [int(input['grade'])],
+                'CNDTN' : [int(input['condition'])],
+                'ROOF' : [int(input['roof'])],
+                'SALEYEAR' : [int(input['sale_year'])]
+            })
+            
+    except:
+        return predict_error()
+    
+    else:
+        if request.method == 'POST':
+            input = request.form
+    
+            df_predict = pd.DataFrame({
+                'BATHRM' : [int(input['bathrooms'])],
+                'HF_BATHRM' : [int(input['half_bathrooms'])],
+                'ROOMS' : [int(input['rooms'])],
+                'BEDRM' : [int(input['bedrooms'])],
+                'ayb_age' : [int(input['building_age'])],
+                'eyb_age' : [int(input['renovation_years'])],
+                'GBA' : [float(input['gba'])],
+                'KITCHENS' : [int(input['kitchens'])],
+                'FIREPLACES' : [int(input['fireplaces'])],
+                'LANDAREA' : [float(input['landarea'])],
+                'LATITUDE' : [float(input['latitude'])],
+                'LONGITUDE' : [float(input['longitude'])],
+                'AC' : [input['ac']],
+                'QUALIFIED' : [input['qualified']],
+                'WARD' : [input['ward']],
+                'QUADRANT' : [input['quadrant']],
+                'HEAT' : [input['heat']],
+                'STYLE' : [input['style']],
+                'USECODE' : [input['usecode']],
+                'STRUCT' : [int(input['struct'])],
+                'GRADE' : [int(input['grade'])],
+                'CNDTN' : [int(input['condition'])],
+                'ROOF' : [int(input['roof'])],
+                'SALEYEAR' : [int(input['sale_year'])]
+            })
+            
+        prediction = int(model.predict(df_predict)[0])
+
+        mae = count_mae()
+    
+        return render_template('result.html', spec=input, pred_result=prediction, mae=mae)
+
+
+@app.route('/data', methods=['GET', 'POST'])
+def data():
+    datamap = show_map()
+    databar_ward = show_bar_ward()
+    databar_ward_count = show_bar_ward_count()
+    databar_grade = show_bar_grade() 
+    databar_grade_count = show_bar_grade_count()
+    databar_usecode = show_bar_usecode()
+    dataline_sale = show_line_sale()
+
+    return render_template('data.html', 
+                           datamap = datamap,
+                           databar_ward=databar_ward, 
+                           databar_ward_count=databar_ward_count,
+                           databar_grade = databar_grade, 
+                           databar_grade_count = databar_grade_count,
+                           databar_usecode = databar_usecode,
+                           dataline_sale=dataline_sale)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/predict-error', methods=['GET', 'POST'])
+def predict_error():
+    return render_template('predict-error.html')
+
+
+if __name__ == '__main__':
+    model_filename = '../Model/Final_Model_RF.sav'
+    model = pickle.load(open(model_filename, 'rb'))
+
+    app.run(debug=True)
+
+    
+
---
+++ Dashboard/model_plots.py
@@ -0,0 +1,194 @@
+import pickle
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly
+import plotly.graph_objs as go
+import json
+import folium
+from folium.features import DivIcon
+
+df = pd.read_csv('../Data/DF_Residential_Analysis.csv')
+
+def show_map():
+    df_ward = df[['WARD', 'PRICE']].groupby(['WARD']).median().reset_index().rename(columns={'WARD':'Ward',
+                                                                                             'PRICE':'Median Price'})
+    
+    with open('../Data/ward-2012.geojson') as f:
+        geojson_data = json.load(f)
+    
+    fig = go.Figure(data=go.Choropleth(geojson=geojson_data,
+                                        colorscale='BuPu',
+                                        locations=df_ward['Ward'].astype(str),
+                                        featureidkey = "properties.NAME",
+                                        z = df_ward['Median Price'].astype(float)))
+    
+    fig.update_geos(fitbounds="locations", visible=False)
+    fig.update_layout(title_text = 'Washington DC Ward Map',
+                      geo_scope='usa', # limit map scope to USA
+                      font_family="Open Sans",
+                      title_font_family="Open Sans"
+                      )
+    
+    fig_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return fig_json
+    
+
+def show_bar_ward():
+    # Version 1 
+    
+    df_ward = df[['WARD', 'PRICE']].groupby(['WARD']).median().reset_index().rename(columns={'WARD':'Ward',
+                                                                                            'PRICE':'Median Price'})
+    
+    fig = px.bar(df_ward,
+                 x='Ward',
+                 y='Median Price',
+                 barmode='group',
+                 color_discrete_sequence=["darkslateblue"],
+                 title='Median Property Price by Ward'
+                 )
+    fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+                       'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+                       'title_font_family':'Open Sans'
+                       })
+    
+    # Version 2
+    '''
+    df_ward = df[['WARD', 'PRICE']].groupby(['WARD']).median().reset_index()
+    fig = go.Figure(data=go.Bar(x=df_ward['WARD'], 
+                                    y=df_ward['PRICE'], 
+                                    marker=dict(color='darkslateblue')
+                                    ))
+    fig.update_layout(title ='Median Property Price by Ward', 
+                      xaxis = {'showgrid': False,
+                                'title':'Ward'},
+                      yaxis = {'showgrid': False,
+                               'title':'Median Price'},
+                      plot_bgcolor = 'rgba(0, 0, 0, 0)',
+                      paper_bgcolor = 'rgba(0, 0, 0, 0)'
+                      )
+    '''
+    
+    fig_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return fig_json
+
+
+
+def show_line_sale():
+    df_sale_year = df[['SALEYEAR', 'PRICE']].groupby(['SALEYEAR']).median().reset_index()
+    
+    fig = go.Figure(data=go.Scatter(x=df_sale_year['SALEYEAR'], 
+                                    y=df_sale_year['PRICE'], 
+                                    mode='lines', 
+                                    marker=dict(color='darkslateblue')
+                                    ))
+    fig.update_layout(title ='Median Property Price Each Year', 
+                      xaxis = {'showgrid': False,
+                                'title':'Sale Year'},
+                      yaxis = {'showgrid': False,
+                               'title':'Median Price'},
+                      plot_bgcolor = 'rgba(0, 0, 0, 0)',
+                      paper_bgcolor = 'rgba(0, 0, 0, 0)'
+                      )
+    
+    fig_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return fig_json
+
+
+def show_bar_ward_count():
+    
+    df_ward = df[['WARD', 'PRICE']].groupby(['WARD']).count().reset_index().rename(columns={'WARD':'Ward',
+                                                                                            'PRICE':'Count'})
+    
+    fig = px.bar(df_ward,
+                 x='Ward',
+                 y='Count',
+                 barmode='group',
+                 color_discrete_sequence=["darkslateblue"],
+                 title='The Number of Properties by Ward'
+                 )
+    fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+                       'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+                       'title_font_family':'Open Sans'
+                       })
+    
+    fig_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return fig_json
+
+def show_bar_grade():
+    # Version 1 
+    
+    df_grade = df[['GRADE', 'PRICE']].groupby(['GRADE']).median().reset_index().rename(columns={'GRADE':'Grade',
+                                                                                               'PRICE':'Median Price'})
+    
+    fig = px.bar(df_grade,
+                 x='Grade',
+                 y='Median Price',
+                 barmode='group',
+                 color_discrete_sequence=["darkslateblue"],
+                 title='Median Property Price by Grade',
+                                  category_orders={'Grade':['Fair Quality',
+                                           'Average',
+                                           'Above Average',
+                                           'Good Quality',
+                                           'Very Good',
+                                           'Excellent',
+                                           'Superior']}
+                 )
+    fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+                       'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+                       'title_font_family':'Open Sans'
+                       })
+    
+    fig_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return fig_json
+
+def show_bar_grade_count():
+    
+    df_grade = df[['GRADE', 'PRICE']].groupby(['GRADE']).count().reset_index().rename(columns={'GRADE':'Grade',
+                                                                                              'PRICE':'Count'})
+    
+
+    
+    fig = px.bar(df_grade,
+                 x='Grade',
+                 y='Count',
+                 barmode='group',
+                 color_discrete_sequence=["darkslateblue"],
+                 title='The Number of Properties by Grade',
+                 category_orders={'Grade':['Fair Quality',
+                                           'Average',
+                                           'Above Average',
+                                           'Good Quality',
+                                           'Very Good',
+                                           'Excellent',
+                                           'Superior']}
+                 )
+    fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+                       'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+                       'title_font_family':'Open Sans'
+                       })
+    
+    fig_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return fig_json
+
+def show_bar_usecode():
+    
+    df_usecode = df[['USECODE', 'PRICE']].groupby(['USECODE']).median().reset_index().rename(columns={'USECODE':'Use Code',
+                                                                                                    'PRICE':'Median Price'})
+    df_usecode['Use Code'] = df_usecode['Use Code'].astype('str')
+    
+    fig = px.bar(df_usecode,
+                 x='Use Code',
+                 y='Median Price',
+                 barmode='group',
+                 color_discrete_sequence=["darkslateblue"],
+                 title='Median Property Price by Use Code'
+                 )
+    fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+                       'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+                       'title_font_family':'Open Sans'
+                       })
+    
+    fig_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return fig_json
